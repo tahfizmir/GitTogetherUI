2e0f00bcbc1ca1def8faab68dd7fc2df15c40346
tahfizmir <tahfeezmir27@gmail.com>
2025-09-30 20:12:39 +0530

    calling the login api, some ui fixes.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("elon@rocky.co"); // initially put for testing
+  const [password, setPassword] = useState("Elon@1234");
 
-export default Login
+  const handleLogin=async()=>{
+    try{
+      const res=await axios.post("http://localhost:3000/login",{
+        emailId,password
+      },{withCredentials:true});   // to set cookies etc
+      console.log(res);
+    }catch(err){
+      console.log(err.message);
+    }
+  }
+  return (
+    <div className="flex justify-center justify-evenly">
+      <div className="card w-96 bg-base-100 card-l shadow-sm my-15 border-1 shadow-2xl ">
+        <div className="card-body shadow-2xl">
+          <h2 className="card-title">Login to proceed</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Enter email</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input"
+                placeholder="Type your email here."
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Enter password</legend>
+              <input
+                type="password"
+                className="input"
+                placeholder="Type your password here."
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary mx-4 my-2" onClick={handleLogin}>LOGIN</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
